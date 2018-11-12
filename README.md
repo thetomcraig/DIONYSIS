a0ea529ad408f4b0d38aa14c08f125e357a02176
Tom Craig <thetomcraig@icloud.com>
2018-11-12 12:22:31 -0800

    Started project, new_project

--- README.md
+++ README.md
@@ -7,53 +7,20 @@ Creating new projects shouldn't be boring!
 Castor can create (or `cast`) new projects like a champ.  
 
 ## Features
-Castor performs the following steps:  
-  * Creates a new folder with the project name specified
-  * `Casts` the new project using one of the available `molds`
-    * Moves the default files into the new projects folder
-    * Commits them with git
-    * Pushes to your GitHub account; creating the remote repo
-
-Curently the following molds exist, With sensible defaults:
+* Castor can create a git-enabled starter project using one of the following schemas (or `molds`):  
   * Python
-    * requirements.txt
   * Bash/sh
-    * Helper Functions
-  * Vimscript
-    * Helper Functions
   * Plain Text
-
-Each new project casted will also contain:
-  * README.md
-  * .gitignore
-  * .ackrc
-  * .agignore
-  * .editorconfig
+* After making this project locally, Castor can push it to a remote location like GitHub
+* For more detail see the [Molds](#molds) section
 
 ## Quick Start
-Castor has `molds` for each type of project.  
-Castor with `cast` a new project using the mold you specify.  
-For example, making a new python project:  
-```
-$ ./castor --cast test_project
-What type of project is this?
-  [1] Python
-  [2] Bash
-  [3] Plain Text (Default)
-$ 1
-Casting from mold...
-Casted
-Pushing cast...
-Enter host password for user 'thetomcraig':$
-.
-.
-.
-Cast pushed
-Opening remote...
-```
+* Castor has `molds` for each type of project.  
+  * These can be viewed in the `molds` folder
+* When you `cast` a new project, Castor will use one of these molds as a base
+* Example: Python
 
-## Usage
-
+## Molds
 
 ### Credits
 <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
--- castor_helper_functions.sh
+++ castor_helper_functions.sh
@@ -4,6 +4,15 @@
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "${CWD}"/bash_helper_functions.sh
 
+#######################################
+# Create new project
+#   Create list of options for user
+#   Initialize project with that choice
+# Globals:
+# Arguments:
+#   project name
+# Returns:
+#######################################
 cast_new_project() {
   project_name="${1}"
 
@@ -21,22 +30,39 @@ cast_new_project() {
   # TODO double check with user
   
   # Create new dir with the given name
-  echo -n "Casting from mold..."
+  echo "Casting from mold..."
   initialize_project_from_mold "${type_from_user}" "${project_name}"
   echo "DONE"
 
-  echo "Pushing remote..."
-  stage_and_push_to_github "${project_name}"
+  echo "Staging commit..."
+  stage_first_commit "${project_name}"
   echo "DONE"
 
+  echo "Creating remote repository..."
+  echo -n "    "
+  create_remote_git_repo "${project_name}"
+  echo "DONE"
+
+  echo "Pushing local repository to remote..."
+  push_to_github
+  echo "DONE"
 }
 
+#######################################
+# Heavy lifting for making the new 
+# project's folder structure
+# Globals:
+# Arguments:
+#   project type
+#   project name
+# Returns:
+#######################################
 initialize_project_from_mold() {
   type="${1}"
   name="${2}"
 
   # Init git and copy appropriate mold
-  # Copy the mold from the castor molds
+  # "mold" is the basic starter files for the project
   cp -r "${CWD}"/molds/"${type}" ./"${name}"
   cp -r "${CWD}"/molds/.editorconfig .
   # Enter the new folder and initialize git
@@ -44,31 +70,48 @@ initialize_project_from_mold() {
   {
     git init
   } &> /dev/null
+
   # Update the text of the readme file; this will be the first commit
-  # This is an array of replacements for the new readme
+  # This associative array holds key/value pairs for making word replacements in the README.md file
   declare -A replacement_dictionary
   replacement_dictionary["<PROJECT NAME>"]="${project_name}"
-
   update_text_file "README.md" "replacement_dictionary"
-  # Run specific mold script
+
+  # All the files are in place, run the remaining initializing logic
+  #   (install libraries, make additional files, etc)
   if [ -f initialize_project.sh ]; then
     ./initialize_project.sh "${name}"
     rm initialize_project.sh
   fi
-
 }
 
 # TODO; get rid of all the output from git stuff
-stage_and_push_to_github() {
+#######################################
+# This should be run after all the 
+# files are in place locally for the 
+# new project
+# This will stage a new (init) commit
+#######################################
+stage_first_commit() {
+  # Stage a commit with the local file
+  {
+    git add .
+    git commit -m "Started project, ${name}" >>/dev/null
+  } &>/dev/null
+}
+
+#######################################
+# Create a repo on GitHub using curl
+#######################################
+create_remote_git_repo() {
   name="${1}"
 
   # Get the credentials needed to push
   github_username=$(get_saved_setting "github_username.txt" "GitHub username?] ")
   otp_code=$(get_saved_setting "github_otp_code.txt" "OTP (2FA) Code for GitHub? (Optional)] ")
-  echo "${otp_code}"
   # Construct the curl command used to push
-  # Ugly backslashed are for quote escaping in json
-  username_section="curl -u ${github_username}" 
+  # Ugly backslashes are for quote escaping in json
+  username_section="curl -s -u ${github_username}" 
   header_section="" 
   if [[ ! -z "${otp_code}" ]]; then
     header_section="-H \"Authorization: token ${otp_code}\""
@@ -77,18 +120,21 @@ stage_and_push_to_github() {
   url_section="https://api.github.com/user/repos"
   # Execute the github curl command
   github_curl_command="${username_section} ${header_section} ${data_section} ${url_section}"
-  echo "${github_curl_command}"
-  eval "${github_curl_command}"
-
-  # Now that the remote GitHub repo hasbeen created, associate with the local git root
-  git remote add origin git@github.com:"${github_username}"/"${name}".git
-  # Stage a commit with the local file
   {
-    git add .
-    git commit -m "Started project, ${name}" >>/dev/null
-  } &>/dev/null
-  # Finally, push the local commit
-  git push -u origin master
+    eval "${github_curl_command}"
+  } > /dev/null
+
+  # Now that the remote GitHub repo hasbeen created, associate with the local git project
+  # Finally, push
+  {
+    git remote add origin git@github.com:"${github_username}"/"${name}".git
+  } &> /dev/null
+}
+
+push_to_github() {
+  {
+    git push -u origin master
+  } &> /dev/null
 }
 
 open_on_github() {
